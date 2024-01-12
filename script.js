aa457767d2ff55f4781f0dbd538354f34da018d7
Abiolayoung <abiolayoung229@gmail.com>
2024-01-12 14:59:19 +0100

    code

---
+++ script.js
@@ -0,0 +1,24 @@
+// data types NUMBER
+// primitive data types- primitive is basically the value of a primitve type, there are 7 primitive types in javascript
+// numbers, string, bigint, null, boolean, symbol, and undefined
+
+// object is a non primitve data type because it is capable of storing multiply values as properties
+// function can be stored in an object e.g
+
+// let john = {
+//     name: 'john',
+//     sayHi: function(){
+//         alert('hey buddy')
+//     }
+// }
+
+// primitive data types provide different set of method- method are inbuilt javascript
+// function that can called when a towards achieving a specific task
+
+// let name = "abiola";
+// alert(name.toUpperCase())
+
+// let num = 1.4353;
+// alert(num.toFixed(2)) //the toFixed method round up the number to 2 decimal places.
+
+// constructor, string, number, boolean are for internal use only
